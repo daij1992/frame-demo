e38ef8ae0b8ccb8e934142eb44fc43ca0e9d11c1
daijie <799099401@qq.com>
2017-08-10 15:51:30 +0800

    日期工具类和IO流工具类

---
+++ common/src/main/java/com/dj/common/utils/date/DateUtil.java
@@ -0,0 +1,14 @@
+package com.dj.common.utils.date;
+
+/**
+ * Created by daijie on 2017-8-10.
+ */
+public class DateUtil {
+
+
+
+
+
+
+
+}
---
+++ common/src/main/java/com/dj/common/utils/io/IOUtil.java
@@ -0,0 +1,7 @@
+package com.dj.common.utils.io;
+
+/**
+ * Created by daijie on 2017-8-10.
+ */
+public class IOUtil {
+}
